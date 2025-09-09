e55950167f8d6a3ac155b70fce47d38ac1506d63
svante2001 <svante2001@gmail.com>
2025-09-09 09:48:56 +0200

    Wip

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+test -d repos/unikraft || git clone https://github.com/unikraft/unikraft repos/unikraft
+# test -d repos/openlib || git clone https://github.com/JuliaMath/openlibm repos/openlib
+
