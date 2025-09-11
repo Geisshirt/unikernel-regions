686002590bc61ff508899b91182102e6020bc3cc
svante2001 <svante2001@gmail.com>
2025-09-11 09:29:49 +0200

    Fixed setup scripts

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@
 test -d repos/unikraft || git clone https://github.com/unikraft/unikraft repos/unikraft
 test -d repos/openlib || git clone https://github.com/JuliaMath/openlibm repos/openlib
 test -d repos/libs/compiler-rt || git clone https://github.com/unikraft/lib-compiler-rt repos/libs/compiler-rt
-bash unikraft/setup.sh
-(cd unikraft; test -d workdir/libs || mkdir workdir/libs; ln -sfn ../../../repos/libs/compiler-rt workdir/libs/compiler-rt) 
+(cd unikraft; bash setup.sh; test -d workdir/libs || mkdir workdir/libs; ln -sfn ../../../repos/libs/compiler-rt workdir/libs/compiler-rt) 
 
 cat libm-replace/Make_src.files > repos/openlib/src/Make.files
 cat libm-replace/Make_i387.files > repos/openlib/i387/Make.files
@@ -13,8 +12,3 @@ cat libm-replace/Make_ld128.files > repos/openlib/ld128/Make.files
 cat libm-replace/Make.inc > repos/openlib/Make.inc
 
 (cd repos/openlib; make install-static; cp libopenlibm.a ../../unikraft)
-
-
-
-
-
