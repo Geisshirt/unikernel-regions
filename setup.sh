17f5c8c99bd5ea6a7d381744e126389618632d91
svante2001 <svante2001@gmail.com>
2025-09-27 15:54:08 +0200

    Initial external

--- setup.sh
+++ setup.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
+git submodule update --init --recursive
 
-test -d repos/unikraft || git clone https://github.com/unikraft/unikraft repos/unikraft
-test -d repos/libs/lwip || git clone https://github.com/unikraft/lib-lwip repos/libs/lwip
-test -d repos/openlib || git clone https://github.com/JuliaMath/openlibm repos/openlib
+cat libm-replace/Make_src.files > external/openlib/src/Make.files
+cat libm-replace/Make_i387.files > external/openlib/i387/Make.files
+cat libm-replace/Make_ld80.files > external/openlib/ld80/Make.files
+cat libm-replace/Make_ld128.files > external/openlib/ld128/Make.files
+cat libm-replace/Make.inc > external/openlib/Make.inc
 
-cat libm-replace/Make_src.files > repos/openlib/src/Make.files
-cat libm-replace/Make_i387.files > repos/openlib/i387/Make.files
-cat libm-replace/Make_ld80.files > repos/openlib/ld80/Make.files
-cat libm-replace/Make_ld128.files > repos/openlib/ld128/Make.files
-cat libm-replace/Make.inc > repos/openlib/Make.inc
-
-(cd repos/openlib; make install-static; cp libopenlibm.a ../../unikraft)
-(cd repos/openlib; make install-static; cp libopenlibm.a ../../c-http)
-
-
-
-
-
+(cd external/openlib; make install-static; cp libopenlibm.a ../../unikraft)
