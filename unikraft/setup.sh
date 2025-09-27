1c20e9fb740a163876f8c556318bb030cee25745
svante2001 <svante2001@gmail.com>
2025-09-27 17:05:40 +0200

    update unikraft

--- unikraft/setup.sh
+++ unikraft/setup.sh
@@ -5,15 +5,15 @@ check_exists_and_create_symlink()
     path="$1"
 
     if ! test -d workdir/"$path"; then
-        if ! test -d ../repos/"$path"; then
-            echo "No directory ../repos/$path. Run the top-level setup.sh script first." 1>&2
+        if ! test -d ../external/"$path"; then
+            echo "No directory ../external/$path. Run the top-level setup.sh script first." 1>&2
             exit 1
         fi
         depth=$(echo "$path" | awk -F / '{ print NF }')
         if test "$depth" -eq 1; then
-            ln -sfn ../../repos/"$path" workdir/"$path"
+            ln -sfn ../../external/"$path" workdir/"$path"
         elif test "$depth" -eq 2; then
-            ln -sfn ../../../repos/"$path" workdir/"$path"
+            ln -sfn ../../../external/"$path" workdir/"$path"
         else
             echo "Unknown depth of path $path." 1>&2
             exit 1
